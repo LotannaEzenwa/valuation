df1cdfa2fae4c306f2e83510b695bef640e4bde1
LotannaEzenwa <L.17@dartmouth.edu>
2016-01-22 04:27:22 -0600

    Grab stock Matlab code finished
    ITS ALIVE :3

--- util/matlab/importStock.m
+++ util/matlab/importStock.m
@@ -1,12 +1,35 @@
-function [ output_args ] = importStock( input_args )
+function [ output_args ] = importStock(ticker)
 %IMPORTSTOCK Grabs a stock's data from storage
 %   Assumes a static filestructure. Here, it's in ../../data
+cf = pwd;
+clean = 'make clean';
+make = 'make';
+
+exe='./stock';
+dn = '/../quandl/';
+data = '/../../data/';
+ext = '.csv';
+
+s = strcat(cf,data,ticker,ext);
 
 
+if exist(s,'file') == 2
+    cd(strcat(pwd,data))
+    sheet = readtable(strcat(ticker,ext));
+else
+    cd(strcat(cf,dn));
+    if exist([pwd '/' exe],'file') ~= 2
+        system(make)
+    end
+    system([exe,' ',ticker])
+    cd(strcat(pwd,data));
+    sheet = readtable(strcat(ticker,ext));
+    
+    
+end
 
-assert(isdir('../../data'));
+cd(cf)
 
-
-output_args = 0;
+output_args = flipud(sheet);
 end
 
